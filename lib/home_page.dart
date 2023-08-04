956c356a25e822b62bab834aca7e9cb88cd24027
azan3050 <mohammadazanansari@gmail.com>
2023-08-04 16:44:45 +0530

    changed hut.dart to home_tab.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,9 +3,9 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:new_app/search.dart';
 import 'package:new_app/settings_page.dart';
 import 'alexaaa.dart';
-import 'hut.dart';
+import 'home_tab.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
   @override
@@ -13,11 +13,10 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _page = 0;
 
   static List<Widget> _widgetOptions = <Widget>[
-    Hut(),
+    HomeTab(),
     Find(),
     AI(),
     Settings(),
@@ -32,8 +31,8 @@ class _HomePageState extends State<HomePage> {
       ),
       bottomNavigationBar: Padding(
         padding: EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 5,
+          horizontal: 15,
+          vertical: 5,
         ),
         child: GNav(
           backgroundColor: Colors.transparent,
@@ -42,20 +41,32 @@ class _HomePageState extends State<HomePage> {
           tabBackgroundColor: Color.fromRGBO(148, 59, 59, 1.0),
           gap: 10,
           selectedIndex: _page,
-          onTabChange: (index){
+          onTabChange: (index) {
             setState(() {
               _page = index;
             });
           },
           padding: EdgeInsets.all(12),
           tabs: [
-          GButton(icon: Icons.home_filled, text: 'Home',),
-          GButton(icon: Icons.search_sharp, text: 'Find',),
-          GButton(icon: Icons.mic, text: 'Ask AI',),
-          GButton(icon: Icons.settings, text: 'Setting',),
-        ],),
+            GButton(
+              icon: Icons.home_filled,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.search_sharp,
+              text: 'Find',
+            ),
+            GButton(
+              icon: Icons.mic,
+              text: 'Ask AI',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Setting',
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
